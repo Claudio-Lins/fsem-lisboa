5dc381c641ff6dc80874d72a01d42525dfb9ac04
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-02-12 18:50:14 +0000

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import '../styles/globals.css'
 
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
-
-// export default MyApp
-
-
 const Layout = ({ children }) => <div className="layout">{children}</div>
 
 export default function App({ Component, pageProps }) {
